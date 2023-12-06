9311ec7e9deb1be2a8ceb68a2c90a5cc8f82eb5e
Aryankashyap266 <99300501+Aryankashyap266@users.noreply.github.com>
2023-12-06 23:52:08 +0530

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect("mongodb://127.0.0.1:27017/Login")
+mongoose.connect("mongodb+srv://aryankashyap:24Dnptest@cluster0.zcaqhpb.mongodb.net/")
 
 .then(()=>{
     console.log("mongodb connected");
